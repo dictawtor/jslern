008c9c40d6fdad498dab289977689f17a9beae30
morteza azami <71493723+dictawtor@users.noreply.github.com>
2024-12-27 18:20:03 +0330

    Add files via upload

---
+++ js learn/main.js
@@ -0,0 +1,89 @@
+// تمرین ترو و فالس
+
+// const price1 = 10;
+// const price2 = 15;
+// const price3 = 4000;
+// const balance = 500;
+// console.log("your account balance is :", balance);
+// const totalPrice = price1 + price2 + price3 * 2;
+
+// console.log("TotalPrice :", totalPrice);
+// const avragePrice = totalPrice / 4;
+// console.log("Avrageprice :", avragePrice);
+// const finalPrice = totalPrice * 0.95;
+// console.log("Fainalprice :", finalPrice);
+// const canBuy = balance >= finalPrice;
+// // console.log("Can buy : " , canBuy);
+// const message = canBuy
+//   ? "horaaa you can buy"
+//   : " sorry your account balance is not enough";
+// console.log(message);
+
+// // متد های toFixde و toString
+// const number = 50;
+// console.log(number);
+// console.log(typeof number);
+
+// const mesal = number.toString();
+// console.log(mesal);
+// console.log(typeof mesal);
+// // متد toFixed
+// const number2 = 50.1354654464164;
+// console.log(number2.toFixed(3));
+
+
+// challange: Capitalize first letter of a text
+// const text = "hi ; i am morteza and im a developer"
+// const firstLetter = text.charAt(0)
+// const upperCaseFirstLetter = firstLetter.toUpperCase()
+// const finaltext = text.replace(firstLetter, upperCaseFirstLetter)
+// console.log(finaltext);
+
+
+// chalange : replace a part op phone number
+// const phone = "09011546532"
+// const substring = phone.slice(3,7)
+// const signs = "****"
+// const finalephone = phone.replace(substring ,signs)
+// console.log(finalephone);
+
+
+// // template literals
+// const name = "morteza"
+// const age  = "26"
+// // const text = "Hello my name is morteza "
+// const text = `Hello my name is ${name} and im ${age} years old`
+// console.log(text);
+
+
+// challange  Extract name an last name
+// const fullname = "Morteza Azami"
+// const spaceIndex = fullname.indexOf(" ")
+// const name = fullname.slice(0 , spaceIndex)
+// const lastName = fullname.slice(spaceIndex +1)
+// const text = `Your name is ${name } and your last name is ${lastName}`
+// console.log(text);
+// console.log(spaceIndex);
+
+
+// Array
+  const myArray = ["morteza", "azami" ,26]
+// console.log(myArray);
+// console.log(myArray[1]);
+//  const string = myArray.join( )
+//  const string1 = myArray.join( " and ")
+//  const Array2 = [1,12,3]
+// const newArray = myArray.concat(Array2)
+// //concat ezaFE MIKONE be array asli
+// console.log(newArray);
+// const new1 = [[ "qwa" , "as"  , "aa"  ] , [ "df1" ,23 ,23, 2] ]
+// const finalarray = new1.flat ( )
+// console.log(finalarray);
+  
+//includes / index of
+  // console.log(myArray.indexOf(26));
+  // console.log(myArray.includes(26));
+// ba include mitunim befahmim ye array vjod dare ya na  va ba index makanesho peyda mikonim
+
+ 
+
