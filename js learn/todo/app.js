d44d8efc9b2b7fdf09c20c8706e0f51664e46398
morteza azami <71493723+dictawtor@users.noreply.github.com>
2025-11-09 21:19:22 +0330

    todo locale storage

--- js learn/todo/app.js
+++ js learn/todo/app.js
@@ -1,1 +1,46 @@
-console.log("Hello World");
+const taskInput = document.getElementById("task-input");
+const dateInput = document.getElementById("date-input");
+const addbutton = document.getElementById("add-button");
+const alertMessage = document.getElementById("alert-message");
+
+const todos = [];
+const generateId = () => {
+  return Math.round(Math.random() * Math.random()*Math.pow(10, 15)).toString();
+  
+}
+
+const showAlert = (message, type) => {
+  alertMessage.innerHTML = "";
+  const alert = document.createElement("p");
+  alert.innerText = message;
+  alert.classList.add("alert");
+  alert.classList.add(`alert-${type}`);
+  alertMessage.append(alert);
+
+  setTimeout(() => {
+    alert.style.display = "none";
+  }, 2000);
+};
+
+const 
+const addHandler = () => {
+  const task = taskInput.value;
+  const date = dateInput.value;
+  const todo = {
+    id : generateId(), 
+    task,
+    date,
+    completed: false,
+  };
+  if (task) {
+    todos.push(todo);
+    taskInput.value = "";
+    dateInput.value = "";
+    console.log(todos);
+    showAlert("todo added sucssesfuly " , "success")
+  } else {
+    showAlert("Please enter a todo! ", "error")
+  }
+};
+addbutton.addEventListener("click", addHandler);
+ 
