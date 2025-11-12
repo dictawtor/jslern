c3927c9ee0dc96107f97aa163223bfafe10104cf
morteza azami <71493723+dictawtor@users.noreply.github.com>
2025-11-12 23:30:40 +0330

    todo app v0.1

--- js learn/todo/app.js
+++ js learn/todo/app.js
@@ -1,9 +1,11 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addbutton = document.getElementById("add-button");
+const editbutton = document.getElementById("edit-button");
 const alertMessage = document.getElementById("alert-message");
 const todosBody = document.querySelector("tbody");
 const deleteAllButton = document.getElementById("deleteAll");
+const filterButtons = document.querySelectorAll(".liter-todos");
 
 let todos = JSON.parse(localStorage.getItem("todos")) || [];
 
@@ -41,11 +43,12 @@ const displayTodos = () => {
     <tr>
           <td>${todo.task}</td>
             <td>${todo.date || "NO date"}</td>
-            <td>${todo.comleted ? "Completed" : "pending"}</td>
+            <td>${todo.completed ? "Completed" : "pending"}</td>
             <td>
-                <button>Edit</button>
-                <button>DO</button>
-                <button onclick="deleteHandler()">Delete</button>
+                <button  onclick="editHandler('${todo.id}')">Edit</button>
+                <button onclick="doHandler('${todo.id}')">
+                ${todo.completed ? "Undo" : "Do"}</button>
+                <button onclick="deleteHandler('${todo.id}')">Delete</button>
           </td>
     </tr> `;
   });
@@ -83,10 +86,82 @@ let deleteAllHandler = () => {
     showAlert("No todos to clear", "error");
   }
 };
-const deleteHandler = () =>{ 
-  console.log("object");
-
+const deleteHandler = (id) => { 
+  todos = todos.filter(todo => todo.id !== id);
+  saveTolocalstorage();
+  displayTodos();
+  showAlert("Todo deleted successfully", "success");
 }
+
+const doHandler = (id) => {
+  // todos = todos.map(todo => {
+  //   if (todo.id === id) {
+  //     return { ...todo, completed: !todo.completed };
+  //   }
+  //   return todo;
+  // });
+  const todo = todos.find(todo => todo.id === id);
+  todo.completed = !todo.completed;
+  saveTolocalstorage();
+  displayTodos();
+  showAlert("Todo status updated successfully", "success");
+};
+
+const editHandler = (id) => {
+  const todo = todos.find(todo => todo.id === id);
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addbutton.style.display = "none";
+  editbutton.style.display = "inline-block";
+  editbutton.onclick = () => {
+    todo.task = taskInput.value;
+    todo.date = dateInput.value;
+    saveTolocalstorage();
+    displayTodos();
+    showAlert("Todo edited successfully", "success");
+    taskInput.value = "";
+    dateInput.value = "";
+    addbutton.style.display = "inline-block";
+    editbutton.style.display = "none";
+  };
+};
+
+
+
+ 
+
+filterButtons.forEach(button => {
+  button.addEventListener("click", () => {
+    const filter = button.innerText.toLowerCase();
+    let filteredTodos = [];
+    if (filter === "all") {
+      filteredTodos = todos;
+    } else if (filter === "pending") {
+      filteredTodos = todos.filter(todo => !todo.completed);
+    } else if (filter === "completed") {
+      filteredTodos = todos.filter(todo => todo.completed);
+    }
+    todosBody.innerHTML = "";
+    if (!filteredTodos.length) {
+      todosBody.innerHTML = "<tr><td colspan='4'>No Task Found</td></tr>";
+      return;
+    }
+    filteredTodos.forEach(todo => {
+      todosBody.innerHTML += `
+      <tr>
+            <td>${todo.task}</td>
+              <td>${todo.date || "NO date"}</td>
+              <td>${todo.completed ? "Completed" : "pending"}</td>
+              <td>
+                  <button  onclick="editHandler('${todo.id}')">Edit</button>
+                  <button onclick="doHandler('${todo.id}')">
+                  ${todo.completed ? "Undo" : "Do"}</button>
+                  <button onclick="deleteHandler('${todo.id}')">Delete</button>
+            </td>
+      </tr> `;
+    });
+  });
+});
 
 window.addEventListener("load", displayTodos);
 addbutton.addEventListener("click", addHandler);
